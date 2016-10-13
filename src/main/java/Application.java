3c94e6c39072a470e775f64e3fed9c2e4c5faef2
HueSpring <xuanhue.7176@gmail.com>
2016-10-13 11:32:48 +0700

    Init git for trainee

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+/**
+ * Created by NTXHue on 10/11/2016.
+ */
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+    }
+}
---
+++ src/main/java/Greeting.java
@@ -0,0 +1,23 @@
+/**
+ * Created by NTXHue on 10/11/2016.
+ */
+package hello;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
